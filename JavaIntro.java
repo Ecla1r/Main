3ee45789b4dd4cfedfb2fefe66027b69c3bb43d9
Ecla1r <64228504+Ecla1r@users.noreply.github.com>
2020-05-20 21:54:53 +0300

    Introduction (Python Course ITIS 3rd semester)

---
+++ JavaIntro.java
@@ -0,0 +1,11 @@
+public class JavaIntro {
+
+    public static void main (String [] args) {
+
+        System.out.println("Korobov Danil Andreevich\n" +
+                            "Date of Birth - 04/11/2001 (Age 18)\n" +
+                            "City of birth - Kazan\n" +
+                            "School - Lyceum №131 of Kazan\n" +
+                            "University - Higher Institute of Information Technologies and Intelligent Systems, KFU");
+    }
+}
---
+++ PythonIntro.py
@@ -0,0 +1,5 @@
+print("Korobov Danil Andreevich\n" +
+      "Date of Birth - 04/11/2001 (Age 18)\n" +
+      "City of birth - Kazan\n" +
+      "School - Lyceum #131 of Kazan\n" +
+      "University - Higher Institute of Information Technologies and Intelligent Systems, KFU")
