9afda85dd22d58daca631366c32c7eea749273e2
Ecla1r <64228504+Ecla1r@users.noreply.github.com>
2020-07-21 03:08:04 +0300

    Add files via upload
    Final (w/ form record)

--- Site/app/routes/users_routes.js
+++ Site/app/routes/users_routes.js
@@ -1,3 +1,6 @@
+bodyParser = require('body-parser').json();
+var fs = require('fs');
+
 module.exports = function (app) {
 
     app.get('/users', (request, respond) => {
@@ -10,6 +13,31 @@ module.exports = function (app) {
                         "email" : "ecla111r@gmail.com"}
                         ];
         respond.send(JSON.stringify(result));
+    });
+
+    app.post('/register', bodyParser, (request, response) => {
+        let body = request.body;
+        console.log(body);
+        fs.readFile('forms.json', 'utf-8', function(err, data) {
+            if (err) throw err;
+
+            var arrayOfObjects = JSON.parse(data);
+            arrayOfObjects.forms.push(body);
+
+
+            fs.writeFile('forms.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+                if (err) throw err;
+                console.log('Done!')
+            })
+        });
+        response.redirect('/search.html');
+    });
+
+    app.get('/forms', (request, response) => {
+        fs.readFile('forms.json', 'utf8', function(err, contents) {
+            response.setHeader('Content-Type', 'application/json');
+            response.send(contents);
+        });
     });
 
 };
